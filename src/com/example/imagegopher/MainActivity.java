9194fcb4e8dcd8970f86361fda6bddc5ba2584f3
surbhi751986 <surbhi751986@gmail.com>
2014-08-06 15:07:00 -0700

    ImageGopher

--- src/com/example/imagegopher/MainActivity.java
+++ src/com/example/imagegopher/MainActivity.java
@@ -14,42 +14,40 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        SpannableString s = new SpannableString("Image Gopher");
-        s.setSpan(new TypefaceSpan(this, "Choco.otf"), 0, s.length(),
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-     
-        // Update the action bar title with the TypefaceSpan instance
-        android.app.ActionBar actionBar = getActionBar();
-        actionBar.setTitle(s);
-        
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		SpannableString s = new SpannableString("Image Gopher");
+		s.setSpan(new TypefaceSpan(this, "Choco.otf"), 0, s.length(),
+				Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-   
-    
-    public void onSearch(View view){
-    	EditText searchStr = (EditText) findViewById(R.id.srchString);
-    	Intent i = new Intent(this, ImageShowcaseActivity.class);
-    	i.putExtra(ImageShowcaseActivity.SEARCH_KEY, searchStr.getText().toString());
-    	startActivity(i);
-    }
-    
-    public void onSettingsClicked(MenuItem mi){
-		Intent i= new Intent(this, AdvanceMenuActivity.class);
+		// Update the action bar title with the TypefaceSpan instance
+		android.app.ActionBar actionBar = getActionBar();
+		actionBar.setTitle(s);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void onSearch(View view) {
+		EditText searchStr = (EditText) findViewById(R.id.srchString);
+		Intent i = new Intent(this, ImageShowcaseActivity.class);
+		i.putExtra(ImageShowcaseActivity.SEARCH_KEY, searchStr.getText()
+				.toString());
 		startActivity(i);
 	}
-    
-    
+
+	public void onSettingsClicked(MenuItem mi) {
+		Intent i = new Intent(this, AdvanceMenuActivity.class);
+		startActivity(i);
+	}
+
 }
